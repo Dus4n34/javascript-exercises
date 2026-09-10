4c1089faac6d65025ca1137d8951edbcd40894f7
Dus4n34 <139997417+Dus4n34@users.noreply.github.com>
2026-09-10 17:39:36 +0200

    04_sumAll solution

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || a < 0 || !Number.isInteger(b) || b < 0) {
+        return "ERROR";
+    }
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    let sum = 0;
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
