091855975a96600f35b2fb157a1aa76bf54f6b89
Dus4n34 <139997417+Dus4n34@users.noreply.github.com>
2026-09-10 13:02:13 +0200

    03_removeFromArray solution

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, values) {
+    let arr = arguments[0];
+    let toRemove = [];
+    for(let i = 1; i < arguments.length; i++) {
+        toRemove.push(arguments[i]);
+    }
+    return arr.filter(value => !toRemove.includes(value));
 };
 
 // Do not edit below this line
